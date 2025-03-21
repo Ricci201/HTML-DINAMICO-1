96434e02bca308c1b4706f29d17e0b25ed36778b
Ricci201 <lucas.ricci@aluno.senai.br>
2025-03-21 16:54:42 -0300

    Atividades de HTML, CSS e JS

--- Atividade01/js/script.js
+++ Atividade01/js/script.js
@@ -1,6 +1,46 @@
 const trocarImagem = () => {
+  let imagem = document.getElementById("imagem");
 
-    const imagem[
-        Palmeiras.png
-    ]
+  const trocarImagens = [
+    "img/Palmeiras.png",
+    "img/Piquerez.png",
+    "img/rios.jpg",
+    "img/Veiga.png",
+  ];
+
+  let indice = 0;
+
+  let numero = Math.random();
+
+  if (numero > 0.75) indice = 3;
+  else if (numero > 0.5) indice = 2;
+  else if (numero > 0.25) indice = 1;
+
+  imagem.src = trocarImagens[indice];
+};
+
+const calcular = (operador) => {
+  let valor1 = parseFloat(frmCalculadora.inValor1.value);
+  let valor2 = parseFloat(frmCalculadora.inValor2.value);
+  let resultado = 0;
+
+  switch (operador) {
+    case "+":
+      resultado = valor1 + valor2;
+      break;
+
+    case "-":
+      resultado = valor1 - valor2;
+      break;
+
+    case "*":
+      resultado = valor1 * valor2;
+      break;
+
+    case "/":
+      resultado = valor1 / valor2;
+      break;
+  }
+
+  frmCalculadora.inResultado.value = resultado;
 };
